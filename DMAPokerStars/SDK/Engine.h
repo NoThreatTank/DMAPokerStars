cd8593c32d012305a33c5e3c316c67a71a943768
Matej Merc <matej.merc@gmail.com>
2024-06-21 16:18:26 +0200

    first commit

---
+++ DMAPokerStars/SDK/Engine.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "ActorEntity.h"
+#include "EngineStructs.h"
+
+// For these offsets just 7 dumper the game and open Engine_Classes
+class Engine
+{
+
+private:
+	uint64_t OwningActorOffset = 0xA0; // NetConnection -> OwningActor
+	uint64_t MaxPacketOffset = 0xA8; // NetConnection -> MaxPacket
+	uint64_t OwningActor;
+	uint64_t MaxPacket;
+	uint64_t OwningGameInstance = 0x1c8; // World -> OwningGameInstance
+	uint64_t PersistentLevel = 0x38; // World  -> PersistentLevel
+	uint64_t GWorld = 0xEBE0C70;
+	uint64_t LocalPlayers = 0x40; // GameInstance -> LocalPlayers
+	uint64_t PlayerController = 0x38; // Player -> PlayerController
+	uint64_t AcknowledgedPawn = 0x2f8;	
+	uint64_t CameraManager = 0x360; // PlayerController -> PlayerCameraManager
+	uint64_t CameraCachePrivate = 0x0; // PlayerCameraManager -> CameraCachePrivate
+	uint64_t CameraCachePrivateOffset = 0x22D0; // PlayerCameraManager -> CameraCachePrivate
+	CameraCacheEntry CameraEntry; // ScriptStruct Engine.CameraCacheEntry
+	MinimalViewInfo CameraViewInfo; // ScriptStruct Engine.MinimalViewInfo
+	std::vector<std::shared_ptr<ActorEntity>> Actors;
+public:
+	Engine();
+	void Cache();
+	void UpdatePlayers();
+	std::vector<std::shared_ptr<ActorEntity>> GetActors();
+	CameraCacheEntry GetCameraCache();
+	void RefreshViewMatrix(VMMDLL_SCATTER_HANDLE handle);
+	uint32_t GetActorSize();
+
+};
---
+++ README.md
@@ -0,0 +1,28 @@
+# DMAPokerSTars
+Simple DMA ESP For PokerStars
+
+## Showcase
+<p align="Left">
+  <img src="Images/image1.png" />
+</p>
+<p align="Left">
+  <img src="Images/image2.png" />
+</p>
+
+## Instructions
+* [Installation Guide](./Instructions.md)
+<br>
+
+To Update: Unless a major update is pushed only the GWorld will need updating which can be gotten manually or through [Dumper-7](https://github.com/Encryqed/Dumper-7).<br>
+All the other offsets are labelled with their class name and fieldname, just search for them in an sdk dump. 
+
+## Features
+* Nothing yet
+  * Incomplete
+
+## Credits
+* [PCILeech](https://github.com/ufrisk/pcileech)
+* [MemProcFS](https://github.com/ufrisk/MemProcFS)
+* [UnnamedZ03](https://github.com/UnnamedZ03/DBD-external-base)
+* [DMALibrary](https://github.com/Metick/DMALibrary/tree/Master)
+* [ImGui] (https://github.com/ocornut/imgui/tree/master)
